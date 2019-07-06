00e5f56693b14b95a117bd91e10c09fa058558d9
Ahmed Bakr <ahmed.m.ali.bakr@gmail.com>
2019-07-06 04:21:13 -0700

    - Implement RANSAC quiz in 3D plane that we will use in the project.

--- src/quiz/ransac/ransac2d.cpp
+++ src/quiz/ransac/ransac2d.cpp
@@ -61,23 +61,23 @@ pcl::visualization::PCLVisualizer::Ptr initScene()
   	return viewer;
 }
 
-//line coefficents are a, b, c
-//point to compare the distance with the line is defined by: (x, y)
-float getPointDistance(float a, float b, float c, float x, float y)
+//plane coefficents are a, b, c, d
+//point to compare the distance with the line is defined by: (x, y, z)
+float getPointDistance(float a, float b, float c, float d, float x, float y, float z)
 {
-	//apply the equation: d = |Ax + By + C| / sqrt(A^2 + B^2)
-	float d = fabs(a * x + b * y + c) / sqrt(a * a + b * b);
-	return d;
+	//apply the equation: d = |Ax + By + Cz + D| / sqrt(A^2 + B^2 + C^2)
+	float dist = fabs(a * x + b * y + c * z + d) / sqrt(a * a + b * b + c * c);
+	return dist;
 }
 
-std::vector<float> findLineCoefficents(float x1, float y1, float x2, float y2)
+std::vector<float> findLineCoefficents(float x1, float y1, float z1, float x2, float y2, float z2, float x3, float y3, float z3)
 {
-	//line equation Ax + By + C = 0
-	//line equation (y1 - y2)x + (x2 - x1)y + (x1 * y2 - x2 * y1) = 0
-	float a = y1 - y2;
-	float b = x2 - x1;
-	float c = x1 * y2 - x2 * y1;
-	std::vector<float> lineCoefficents = {a, b, c};
+	//plane equation Ax + By + Cz + D = 0
+	float a = (y2-y1) * (z3-z1) - (z2-z1) * (y3-y1);
+	float b = (z2-z1) * (x3-x1) - (x2-x1) * (z3-z1);
+	float c = (x2-x1) * (y3-y1) - (y2-y1) * (x3-x1);
+	float d = -(a * x1 + b * y1 + c * z1);
+	std::vector<float> lineCoefficents = {a, b, c, d};
 	return lineCoefficents;
 }
 
@@ -90,21 +90,28 @@ std::unordered_set<int> Ransac(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int ma
 	// TODO: Fill in this function
 
 	// For max iterations 
-	float point1Idx, point2Idx, maxCountPointsOnLine = 0, bestPoint1Idx = 0, bestPoint2Idx = 0;
+	float point1Idx, point2Idx, point3Idx, maxCountPointsOnLine = 0;
+	float bestPoint1Idx = 0, bestPoint2Idx = 0, bestPoint3Idx = 0;
 	for(int j = 0; j < maxIterations; ++j)
 	{
 		// Randomly sample subset and fit line
 		point1Idx = rand() % cloud->points.size();
-		point2Idx = point1Idx;
+		point3Idx = point2Idx = point1Idx;
 		while(point1Idx == point2Idx)//make sure that the two indices are not equal
 			point2Idx = rand() % cloud->points.size(); 
+		
+		while(point3Idx == point2Idx || point3Idx == point1Idx)//make sure that the two indices are not equal
+			point3Idx = rand() % cloud->points.size(); 
 
 		pcl::PointXYZ point1 = cloud->points[point1Idx];
 		pcl::PointXYZ point2 = cloud->points[point2Idx];
-		std::vector<float> lineCoeffs = findLineCoefficents(point1.x, point1.y, point2.x, point2.y);
+		pcl::PointXYZ point3 = cloud->points[point3Idx];
+		std::vector<float> lineCoeffs = findLineCoefficents(point1.x, point1.y, point1.z,
+		 point2.x, point2.y, point2.z , point3.x, point3.y, point3.z);
 		float a = lineCoeffs[0];
 		float b = lineCoeffs[1];
 		float c = lineCoeffs[2];
+		float d = lineCoeffs[3];
 
 		float countPointsOnLine = 0;
 		// Measure distance between every point and fitted line
@@ -112,8 +119,8 @@ std::unordered_set<int> Ransac(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int ma
 		for(int i = 0; i < cloud->points.size(); ++i)
 		{
 			pcl::PointXYZ aPoint = cloud->points[i];
-			float x = aPoint.x, y = aPoint.y;
-			float dist = getPointDistance(a, b, c, x, y);
+			float x = aPoint.x, y = aPoint.y, z = aPoint.z;
+			float dist = getPointDistance(a, b, c, d, x, y, z);
 			if(dist < distanceTol)
 			{
 				countPointsOnLine++;
@@ -124,22 +131,25 @@ std::unordered_set<int> Ransac(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int ma
 			maxCountPointsOnLine = countPointsOnLine;
 			bestPoint1Idx = point1Idx;
 			bestPoint2Idx = point2Idx;
+			bestPoint3Idx = point3Idx;
 		}
 	}
 	
 	// Return indicies of inliers from fitted line with most inliers
 	pcl::PointXYZ bestPoint1 = cloud->points[bestPoint1Idx];
 	pcl::PointXYZ bestPoint2 = cloud->points[bestPoint2Idx];
-	std::vector<float> bestLineCoeffs = findLineCoefficents(bestPoint1.x, bestPoint1.y,
-	 bestPoint2.x, bestPoint2.y);
+	pcl::PointXYZ bestPoint3 = cloud->points[bestPoint3Idx];
+	std::vector<float> bestLineCoeffs = findLineCoefficents(bestPoint1.x, bestPoint1.y, bestPoint1.z,
+	  bestPoint2.x, bestPoint2.y, bestPoint2.z, bestPoint3.x, bestPoint3.y, bestPoint3.z);
 	float bestA = bestLineCoeffs[0];
 	float bestB = bestLineCoeffs[1];
 	float bestC = bestLineCoeffs[2];
+	float bestD = bestLineCoeffs[3];
 	for(int i = 0; i < cloud->points.size(); ++i)
 	{
 		pcl::PointXYZ aPoint = cloud->points[i];
-		float x = aPoint.x, y = aPoint.y;
-		float dist = getPointDistance(bestA, bestB, bestC, x, y);
+		float x = aPoint.x, y = aPoint.y, z = aPoint.z;
+		float dist = getPointDistance(bestA, bestB, bestC, bestD, x, y, z);
 		if(dist < distanceTol)
 		{
 			inliersResult.insert(i);
@@ -158,11 +168,11 @@ int main ()
 	pcl::visualization::PCLVisualizer::Ptr viewer = initScene();
 
 	// Create data
-	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = CreateData();
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = CreateData3D();
 	
 
 	// TODO: Change the max iteration and distance tolerance arguments for Ransac function
-	std::unordered_set<int> inliers = Ransac(cloud, 500, 0.7);
+	std::unordered_set<int> inliers = Ransac(cloud, 1000, 0.2);
 
 	pcl::PointCloud<pcl::PointXYZ>::Ptr  cloudInliers(new pcl::PointCloud<pcl::PointXYZ>());
 	pcl::PointCloud<pcl::PointXYZ>::Ptr cloudOutliers(new pcl::PointCloud<pcl::PointXYZ>());
